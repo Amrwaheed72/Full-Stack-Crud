61f2e68ef209abfca773b12dc7d367f7396493a6
Amr Waheed <amrmohamed877@gmail.com>
2025-02-03 10:14:35 +0200

    finishing the front-end

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         isOpened={isOpened}
         onClose={() => setIsOpened(false)}
       />
-    </div>
+    </div> 
   );
 }
 
--- Front-End/src/assets/components/ModalForm.jsx
+++ Front-End/src/assets/components/ModalForm.jsx
@@ -1,36 +1,87 @@
+import { useState } from "react";
+
 function ModalForm({ isOpened, onClose, mode, onSubmit }) {
+  const [rate, setRate] = useState(0);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [job, setJob] = useState("");
+  const [status, setStatus] = useState(false);
+
+  function handleStatus(e) {
+    setStatus(e.target.value === "Active");
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      rate,
+      name,
+      email,
+      job,
+      status,
+    };
+    onsubmit = { formData };
+  }
+
   return (
     <div>
-      <dialog id="my_modal_3" className="modal " open={isOpened}>
+      <dialog id="my_modal_3" className="modal" open={isOpened}>
         <div className="modal-box">
           <h3 className="font-bold text-lg py-4">
             {mode === "edit" ? "Edit Client" : "Client Details"}
           </h3>
-          <form  method="dialog">
+          <form onSubmit={handleSubmit} method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <label className=" my-4 input input-bordered flex items-center gap-2">
+            <label className="my-4 input input-bordered flex items-center gap-2">
               Name
-              <input type="text" className="grow" placeholder="Full Name" />
+              <input
+                type="text"
+                className="grow"
+                placeholder="Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </label>
-            <label className="  my-4 input input-bordered flex items-center gap-2">
+            <label className="my-4 input input-bordered flex items-center gap-2">
               Email
-              <input type="text" className="grow" placeholder="Email Address" />
+              <input
+                type="text"
+                className="grow"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </label>
-            <label className="  my-4 input input-bordered flex items-center gap-2">
+            <label className="my-4 input input-bordered flex items-center gap-2">
               Job
-              <input type="text" className="grow" placeholder="Your Job" />
+              <input
+                type="text"
+                className="grow"
+                placeholder="Your Job"
+                value={job}
+                onChange={(e) => setJob(e.target.value)}
+              />
             </label>
-            <div className="flex justify-between mb-4 ">
-              <label className=" input input-bordered flex items-center gap-2">
+            <div className="flex justify-between mb-4">
+              <label className="input input-bordered flex items-center gap-2">
                 Rate
-                <input type="number" className="grow" placeholder="Your Rate" />
+                <input
+                  type="number"
+                  className="grow"
+                  placeholder="Your Rate"
+                  value={rate}
+                  onChange={(e) => setRate(e.target.value)}
+                />
               </label>
-              <select className=" mx-2 select select-bordered w-full max-w-xs">
-                <option disabled selected>
-                  Select Your Stats
+              <select
+                value={status ? "Active" : "Inactive"}
+                className="mx-2 select select-bordered w-full max-w-xs"
+                onChange={handleStatus}
+              >
+                <option disabled value="">
+                  Select Your Status
                 </option>
-                <option>Active</option>
-                <option>inactive</option>
+                <option value="Active">Active</option>
+                <option value="Inactive">Inactive</option>
               </select>
             </div>
             <button
@@ -39,7 +90,7 @@ function ModalForm({ isOpened, onClose, mode, onSubmit }) {
             >
               ✕
             </button>
-            <button className="btn btn-success">
+            <button type="submit" className="btn btn-success">
               {mode === "edit" ? "Save Changes" : "Add Client"}
             </button>
           </form>
